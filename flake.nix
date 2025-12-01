4f871c9fdf754351cf373811af915b9ea74b21e3
John Shaffer <jdsha@pm.me>
2025-11-30 22:20:51 -0600

    Update deps

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "hyperfine";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
