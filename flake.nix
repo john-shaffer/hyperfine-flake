e81a1e934c4ff59116f4de9ed7c3f9852f1cb317
John Shaffer <jdsha@pm.me>
2025-12-27 01:31:21 -0600

    Remove flake-utils dependency
    It's not needed since we can easily implement
    the functionality ourselves.
    
    See https://ayats.org/blog/no-flake-utils

--- flake.nix
+++ flake.nix
@@ -3,109 +3,112 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
+    { nixpkgs, ... }:
+    let
+      supportedSystems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      forAllSystems =
+        function: nixpkgs.lib.genAttrs supportedSystems (system: function nixpkgs.legacyPackages.${system});
+    in
     {
-      self,
-      nixpkgs,
-      flake-utils,
-      ...
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      with import nixpkgs { inherit system; };
-      with pkgs;
-      let
-        version = "1.20.0";
-        src = fetchFromGitHub {
-          owner = "sharkdp";
-          repo = "hyperfine";
-          rev = "v${version}";
-          hash = "sha256-Ee889Fx2Mi2005SrlcKc7TwG8ZIpTqisfLebXYadvSg=";
-        };
-        # From https://github.com/NixOS/nixpkgs/blob/59e69648d345d6e8fef86158c555730fa12af9de/pkgs/by-name/hy/hyperfine/package.nix
-        hyperfine = rustPlatform.buildRustPackage rec {
-          inherit src version;
-          pname = "hyperfine";
+      packages = forAllSystems (
+        pkgs:
+        with pkgs;
+        let
+          version = "1.20.0";
+          src = fetchFromGitHub {
+            owner = "sharkdp";
+            repo = "hyperfine";
+            rev = "v${version}";
+            hash = "sha256-Ee889Fx2Mi2005SrlcKc7TwG8ZIpTqisfLebXYadvSg=";
+          };
+          # From https://github.com/NixOS/nixpkgs/blob/59e69648d345d6e8fef86158c555730fa12af9de/pkgs/by-name/hy/hyperfine/package.nix
+          hyperfine = rustPlatform.buildRustPackage rec {
+            inherit src version;
+            pname = "hyperfine";
 
-          cargoHash = "sha256-0e6QDVv//WQtfvrJj6jW1sEz7jFv3VC6UKLvclyytLs=";
+            cargoHash = "sha256-0e6QDVv//WQtfvrJj6jW1sEz7jFv3VC6UKLvclyytLs=";
 
-          nativeBuildInputs = [ installShellFiles ];
+            nativeBuildInputs = [ installShellFiles ];
 
-          postInstall = ''
-            installManPage doc/hyperfine.1
+            postInstall = ''
+              installManPage doc/hyperfine.1
 
-            installShellCompletion \
-              $releaseDir/build/hyperfine-*/out/hyperfine.{bash,fish} \
-              --zsh $releaseDir/build/hyperfine-*/out/_hyperfine
-          '';
-        };
-        scripts-python = (
-          pkgs.python3.withPackages (
-            python-pkgs: with python-pkgs; [
+              installShellCompletion \
+                $releaseDir/build/hyperfine-*/out/hyperfine.{bash,fish} \
+                --zsh $releaseDir/build/hyperfine-*/out/_hyperfine
+            '';
+          };
+          scripts-python = (
+            pkgs.python3.withPackages (
+              python-pkgs: with python-pkgs; [
+                matplotlib
+                numpy
+                pyqt6
+                scipy
+              ]
+            )
+          );
+          scripts = python3Packages.buildPythonPackage rec {
+            pname = "hyperfine-scripts";
+            inherit src version;
+
+            propagatedBuildInputs = with python3Packages; [
               matplotlib
               numpy
               pyqt6
               scipy
-            ]
-          )
-        );
-        scripts = python3Packages.buildPythonPackage rec {
-          pname = "hyperfine-scripts";
-          inherit src version;
+            ];
 
-          propagatedBuildInputs = with python3Packages; [
-            matplotlib
-            numpy
-            pyqt6
-            scipy
-          ];
+            doCheck = false;
 
-          doCheck = false;
+            buildPhase = ''
+              true
+            '';
 
-          buildPhase = ''
-            true
-          '';
+            format = "other";
 
-          format = "other";
+            installPhase = ''
+              mkdir -p $out/bin
+              mkdir -p $out/share/hyperfine/scripts
 
-          installPhase = ''
-            mkdir -p $out/bin
-            mkdir -p $out/share/hyperfine/scripts
+              # Copy the original plot_*.py scripts
+              cp ${src}/scripts/*.py $out/share/hyperfine/scripts/
 
-            # Copy the original plot_*.py scripts
-            cp ${src}/scripts/*.py $out/share/hyperfine/scripts/
-
-            # For each plot script create a lightweight wrapper in bin/
-            for script in $out/share/hyperfine/scripts/*.py; do
-              name=$(basename "$script" .py)
-              bin_name=$(printf '%s' "$name" | sed 's/_/-/g')
-              wrapper="$out/bin/hyperfine-$bin_name"
-              cat > "$wrapper" <<EOF
-            #!${scripts-python.interpreter}
-            import os, sys
-            script = os.path.join(os.path.dirname(__file__), "..", "share", "hyperfine", "scripts", "$name.py")
-            sys.exit(os.execv(sys.executable, [sys.executable, script] + sys.argv[1:]))
-            EOF
-              chmod +x "$wrapper"
-            done
-          '';
-        };
-        hyperfine-with-scripts = symlinkJoin {
-          name = "hyperfine-with-scripts-${version}";
-          paths = [
-            hyperfine
-            scripts
-          ];
-        };
-      in
-      {
-        packages = {
+              # For each plot script create a lightweight wrapper in bin/
+              for script in $out/share/hyperfine/scripts/*.py; do
+                name=$(basename "$script" .py)
+                bin_name=$(printf '%s' "$name" | sed 's/_/-/g')
+                wrapper="$out/bin/hyperfine-$bin_name"
+                cat > "$wrapper" <<EOF
+              #!${scripts-python.interpreter}
+              import os, sys
+              script = os.path.join(os.path.dirname(__file__), "..", "share", "hyperfine", "scripts", "$name.py")
+              sys.exit(os.execv(sys.executable, [sys.executable, script] + sys.argv[1:]))
+              EOF
+                chmod +x "$wrapper"
+              done
+            '';
+          };
+          hyperfine-with-scripts = symlinkJoin {
+            name = "hyperfine-with-scripts-${version}";
+            paths = [
+              hyperfine
+              scripts
+            ];
+          };
+        in
+        {
           inherit hyperfine scripts hyperfine-with-scripts;
           default = hyperfine-with-scripts;
-        };
-      }
-    );
+        }
+      );
+    };
 }
